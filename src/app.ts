faf7caef3c95402384475d6b20e6585a5ea92b66
highkeen11 <abhijit422003@gmail.com>
2026-07-21 08:08:15 +0530

    allowing end point for all

--- src/app.ts
+++ src/app.ts
@@ -12,10 +12,10 @@ import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 8000
-app.use(cors({
-    origin: ['http://localhost:5173'],
-    credentials: true
-}));
+// app.use(cors({
+//     origin: ['http://localhost:5173'],
+//     credentials: true
+// }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
